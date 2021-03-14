74713902b6320256c4c00d400ffa0e66ac46dd18
Tasos Lelakis <tasospatra@hotmail.com>
2021-03-14 11:44:55 +0200

    Add Concurrency Locks in ShoppingCounterBean

--- WineApp-ejb/src/main/java/emergon/singleton/ShopperCountBean.java
+++ WineApp-ejb/src/main/java/emergon/singleton/ShopperCountBean.java
@@ -2,6 +2,8 @@ package emergon.singleton;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.ejb.Lock;
+import javax.ejb.LockType;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
@@ -17,10 +19,12 @@ public class ShopperCountBean {
         resetCounter();
     }
     
+    @Lock(LockType.WRITE)//Only one client can access this method at a time
     public void incrementNumberOfShoppers(){
         shopperCounter++;
     }
     
+    @Lock(LockType.READ)//Multiple clients cat access this method concurrently
     public int getNumberOfShoppers(){
         return shopperCounter;
     }
