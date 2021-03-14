595a3b85a495fda30278123642f986d5e47f1302
Tasos Lelakis <tasospatra@hotmail.com>
2021-03-14 11:57:10 +0200

    Add @Scheduler for logging shopperCounter in LogShopperCounter

--- WineApp-ejb/src/main/java/emergon/singleton/LogShopperCounter.java
+++ WineApp-ejb/src/main/java/emergon/singleton/LogShopperCounter.java
@@ -1,9 +1,13 @@
 package emergon.singleton;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.DependsOn;
+import javax.ejb.EJB;
+import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.ejb.Timer;
 
 @DependsOn({"ShopperCount"})
 @Startup
@@ -11,4 +15,16 @@ import javax.ejb.Startup;
 public class LogShopperCounter {
     
     private final Logger log = Logger.getLogger("LogShopperCounter.class");
+    
+    @EJB
+    private ShopperCountBean shopperCountBean;
+    
+    //log every 5 minutes
+    @Schedule(minute = "*/5")
+    public void logShopperCounter(Timer timer){
+        String message = ">>>Scheduler running: Shopper online are:"+ shopperCountBean.getNumberOfShoppers();
+        log.log(Level.INFO, message);
+        String timerInfo = (String) timer.getInfo();
+        log.log(Level.INFO, timerInfo);
+    }
 }
